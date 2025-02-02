e518baff569c767525c1dc6be05a344cb04b7c89
VENUCHANAPATHIGIT <venu.chanapathi.git@gmail.com>
2025-02-02 15:03:53 +0530

    Chnaged names & all | ready to produtionn

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -116,8 +116,8 @@
     simplyCountdown = function (elt, args) {
         var parameters = extend({
                 year: 2025,
-                month: 5,
-                day: 28,
+                month: 3,
+                day: 6,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
